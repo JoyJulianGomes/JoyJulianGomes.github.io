376e38218172abb40fa5aea7a357984b87a24b01
Joy Julian Gomes <joyjuliangomes@gmail.com>
2019-01-31 20:13:52 +0600

    portfolio@0.1.0

---
+++ precache-manifest.17c376cde820fbb08fe69a5893e70a9b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "26b5840d3dca86f7c78cc8c2c1043669",
+    "url": "/static/media/logo.26b5840d.jpg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "029eab1fbc98cc7b0cd0",
+    "url": "/static/js/main.029eab1f.chunk.js"
+  },
+  {
+    "revision": "56e412d63f26885c1c3e",
+    "url": "/static/js/1.56e412d6.chunk.js"
+  },
+  {
+    "revision": "029eab1fbc98cc7b0cd0",
+    "url": "/static/css/main.a30c74be.chunk.css"
+  },
+  {
+    "revision": "d1f8bdef6a45cda44bdf1ff77d5495db",
+    "url": "/index.html"
+  }
+];
